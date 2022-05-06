2e9a7823039bbe880214d342725a5468c3aec271
lakshana yathavarayar <yathavarayarlakshana@gmail.fr>
2022-05-06 12:23:27 +0200

    mon premier parseur

---
+++ main.ml
@@ -0,0 +1,20 @@
+(*fichier main.ml *)
+
+(* the arguments that are passed to a given program on the command line are stored in an array. Following tradition, this array is named argv. 
+It is found in the Sys module of the standard library, therefore its full name is Sys.argv. *)
+let _ = 
+  try
+      let lexbuf = Lexing.from_channel stdin in (*lexeur lancé sur stdin*)
+      while true do (*on ne s'arrête pas*)
+          Parseur.main Lexeur.token lexbuf (*parseur une ligne*)
+      done
+  with
+      | Lexeur.Eof -> exit 0 (*impossible*)
+      | Lexeur.TokenInconu (*erreur de lexing*)
+      | Parsing.Parse_error -> (*erreur de parsing*)
+          Printf.printf ("Ceci n'est pas une expression arithmetique\n")
+
+  
+
+
+      
